2bf64194871622a01719ce07bc95298323547972
SashaSheinin <sasha@idomoo.com>
2024-07-13 10:41:28 +0300

    add upload file test

--- new-project/tests/example.spec.ts
+++ new-project/tests/example.spec.ts
@@ -1,9 +1,20 @@
 import { test, expect } from '@playwright/test';
 import * as path from 'path';
 
-test('has title', async ({ page }) => {
+test('Upload folder', async ({ page }) => {
   let html = path.join(__dirname, 'upload_template.html')  
   await page.goto(`file://${html}`);
-  await page.locator('input[type=file]').setInputFiles(path.join(__dirname, 'folder'));
+  let local_path = path.join(__dirname, 'folder');
+  console.log(local_path);
+  await page.locator('input[type=file]').setInputFiles('/Users/sashas/Documents/bug_repo/new-project/tests/folder');
+  console.log('Uploaded')
+});
+
+test('Upload file', async ({ page }) => {
+  let html = path.join(__dirname, 'upload_template.html')  
+  await page.goto(`file://${html}`);
+  let local_path = path.join(__dirname, 'folder/first_file.json');
+  console.log(local_path);
+  await page.locator('input[type=file]').setInputFiles('/Users/sashas/Documents/bug_repo/new-project/tests/folder/first_file.json');
   console.log('Uploaded')
 });
