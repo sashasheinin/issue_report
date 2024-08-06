c68485aa985c1e8f97c364f6b3c3f827d9bc371c
SashaSheinin <sasha@idomoo.com>
2024-08-06 09:38:34 +0300

    fix

--- new-project/tests/example.spec.ts
+++ new-project/tests/example.spec.ts
@@ -4,7 +4,7 @@ import * as path from 'path';
 test('Upload folder', async ({ page }) => {
   let html = path.join(__dirname, 'upload_template.html')  
   await page.goto(`file://${html}`);
-  let local_path = path.join(__dirname, 'folder');
+  let local_path = path.join(__dirname, 'test_f');
   console.log(local_path);
   await page.locator('input[type=file]').setInputFiles(local_path);
   console.log('Uploaded')
@@ -14,11 +14,11 @@ test('Upload file', async ({ page }) => {
   let html = path.join(__dirname, 'upload_template.html')  
   await page.goto(`file://${html}`);
   let local_path = path.join(__dirname, 'test_f');
-  local_path = process.cwd() + '/tests/test_f'
+  local_path = process.cwd() + '/new-project/tests/test_f'
   console.log(local_path);
   const fileChooserPromise = page.waitForEvent('filechooser');
   await page.locator('input[type=file]').click();
   const fileChooser = await fileChooserPromise;
-  await fileChooser.setFiles(local_path);
+  await fileChooser.setFiles('./new-project/tests/test_f');
   console.log('Uploaded')
 });
